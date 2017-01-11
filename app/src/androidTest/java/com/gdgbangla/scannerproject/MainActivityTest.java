9c70bc32315bf3762b5bcdddb556b54284634850
memotiur <memotiur@gmail.com>
2017-01-11 12:43:35 +0600

    For youtube channel

---
+++ app/src/androidTest/java/com/gdgbangla/scannerproject/MainActivityTest.java
@@ -0,0 +1,23 @@
+package com.gdgbangla.scannerproject;
+
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+    }
+
+}
---
+++ app/src/main/java/com/gdgbangla/scannerproject/MainActivity.java
@@ -0,0 +1,40 @@
+package com.gdgbangla.scannerproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+    private ZXingScannerView zXingScannerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void scan(View view){
+        zXingScannerView =new ZXingScannerView(getApplicationContext());
+        setContentView(zXingScannerView);
+        zXingScannerView.setResultHandler(this);
+        zXingScannerView.startCamera();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        zXingScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        Toast.makeText(getApplicationContext(),result.getText(),Toast.LENGTH_SHORT).show();
+        zXingScannerView.resumeCameraPreview(this);
+
+    }
+}
